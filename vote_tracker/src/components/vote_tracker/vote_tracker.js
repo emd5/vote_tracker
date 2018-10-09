05dbdd561525f4336caf7c96906af24c90cfea7f
Stephen <stephendeanharper@gmail.com>
2018-10-08 18:43:14 -0700

    added commented out code with what i thought would work. tried two different ways: calling the decrement in the handleup and handledown function. then creating new method to do the decreasing. no dice so far

--- vote_tracker/src/components/vote_tracker/vote_tracker.js
+++ vote_tracker/src/components/vote_tracker/vote_tracker.js
@@ -9,28 +9,40 @@ class Vote_tracker extends React.Component{
         this.state = {
             counter: 0,
             polarity: 'neutral',
+            hidden_vote_limit: 10,
         }
 
         this.handleUp = this.handleUp.bind(this);
         this.handleDown = this.handleDown.bind(this);
+        // this.decreaseVoteLimit = this.decreaseVoteLimit.bind(this);
         this.updateState = this.updateState.bind(this);
-
     }
 
     handleUp() {
         this.updateState(this.state.counter + 1);
-    }
+        // this.updateState(this.state.hidden_vote_limit - 1);
+        // decreaseVoteLimit()
+        }
 
     handleDown() {
         this.updateState(this.state.counter - 1);
+        // this.updateState(this.state.hidden_vote_limit - 1);
+        // decreaseVoteLimit()
     }
 
-    updateState(counter) {
+    // decreaseVoteLimit() {
+    //     this.updateState(this.state.hidden_vote_limit - 1);
+    // }
+
+    updateState(counter, hidden_vote_limit) {
         let polarity = null;
         if (counter > 0){polarity = "positive"};
         if (counter < 0){polarity = "negative"};
+        // if (hidden_vote_limit = 10) {
+        //     return "No more votes!"
+        // }
         this.setState(
-            {counter, polarity}
+            {counter, polarity, hidden_vote_limit}
         )
     }
 
@@ -44,6 +56,7 @@ class Vote_tracker extends React.Component{
                 <h2 className={this.state.polarity}>{this.state.counter}</h2>
                 <span onClick={this.handleDown}>-</span>
                 <span onClick={this.handleUp}>+</span>
+                {/* <h2>{this.state.hidden_vote_limit}</h2> */}
             </main>
         )
     }
